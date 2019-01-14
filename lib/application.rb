54483a709900cdb8071baf2c4f3b5ed698dea8cc
ewamal <maluseckae@gmail.com>
2019-01-14 14:57:34 +0100

    :lipstick:

--- lib/application.rb
+++ lib/application.rb
@@ -1,18 +1,64 @@
 Dir[File.dirname(__FILE__) + '/application/*.rb'].each { |file| require file }
 
-class Application
-  def initialize
+
+class World
+  attr_accessor :grid
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def start
+    @grid[0][0].makelife
+  end
+
+  def give_lives(*coordinates)
+    coordinates.each do |x,y|
+      @grid[x][y].make_life
+    end
+  end
+  def murder(*coordinates)
+    coordinates.each do |x,y|
+      @grid[x][y].make_dead
+    end
+  end
+end
+
+class Cell
+  attr_accessor :alive
+
+  def initialize(x, y)
+    @alive = false
+    @x = x
+    @y = y
+  end
+
+  def make_life
+    @alive = true
+  end
+
+  def make_dead
+    @alive = false
   end
 end
 
 class Grid
-  def initialize(arr)
-    @arr = arr
-  end
-end
+  attr_accessor :rows, :columns
 
-class Environment
-  def initialize(grid)
-    @grid = grid
+  def initialize(rows=3, columns=3)
+    @rows = rows
+    @columns = columns
+    @grid = Array.new(rows) do |row|
+              Array.new(columns) do |column|
+                puts "row: #{row}"
+                puts "column: #{column}"
+                Cell.new(column, row)
+              end
+            end
   end
+
+  def number_of_living_neigbours(cell)
+
+  end
+
 end
--- spec/unit/environment_spec.rb
+++ spec/unit/environment_spec.rb
@@ -1,14 +1,30 @@
 require 'spec_helper'
 
-describe "Environment" do
-  it "should be empty if there is no grid"do
-    grid = Grid.new([
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-      ])
-    environment = Environment.new(grid)
-
-    expect(environment).to eq(environment.empty?)
+describe "World" do
+  subject { World.new }
+  it 'should create a new world' do
+    expect(subject).to be_instance_of(World)
   end
+
+  it 'should create grid on init' do
+    expect(subject.grid.is_a? Array).to be true
+  end
+
+  it 'grid should have a length of columns' do
+    expect(subject.grid.size).to eq 3
+  end
+
+  context 'cell' do
+    subject { Cell.new }
+    it 'should create a new cell' do
+      expect(subject).to be_instance_of(Cell)
+    end
+
+    it "should init with alive false" do
+      expect(subject.alive).to be false
+    end
+
+  end
+
+
 end
